292823594b10badcf3637bb8f25758b39c10a06d
Keenan <keenanhallas@gmail.com>
2020-04-14 00:48:08 -0400

    preventDefault() & stopPropagation() added

--- js/index.js
+++ js/index.js
@@ -9,21 +9,29 @@ navLinks.forEach(link => {
     link.style.width = "20%";
     link.style.textAlign = "center";
     link.style.borderRadius = "5px"
+    link.addEventListener("click", event => {
+        event.preventDefault();
+    });
     link.addEventListener("mouseover", event => {
         event.target.style.color = "yellow";
         event.target.style.backgroundColor = "powderblue";
         event.target.style.fontSize = "1.9rem";
         event.target.style.lineHeight = "2";
-    })
+    });
     link.addEventListener("mouseout", event => {
         event.target.style.color = "black";
         event.target.style.backgroundColor = "white";
         event.target.style.fontSize = "1.6rem";
         event.target.style.lineHeight = "2.2";
-    })
+    });
 });
 
-const homeImg = document.querySelector(".home img");
+// const introSection = document.querySelector(".intro"); //not working for some reason
+// introSection.addEventListener("click", event => {
+//     event.target.parent.style.backgroundColor = "red";
+// });
+
+const homeImg = document.querySelector(".intro img");
 homeImg.addEventListener("click", event => {
     event.target.style.borderRadius = "15px";
     event.target.style.marginTop = "3.5%";
@@ -42,25 +50,35 @@ imgs.forEach(img => {
     img.addEventListener("dragend", element => {
         event.target.style.display = "block";
     });
-    img.addEventListener("wheel", event => {
-        event.preventDefault();
-        event.target.style.opacity -= 0.01;
-    });
+    // img.addEventListener("wheel", event => { //opacity won't scale
+    //     event.preventDefault();
+    //     event.target.style.opacity -= 0.01;
+    // });
 });
+
+const trips = document.querySelectorAll(".destination");
+trips.forEach(trip => {
+    trip.addEventListener("contextmenu", event => {
+        event.preventDefault();
+        window.alert("This window won't appear when the button below is alt-clicked, due to event propagation being stopped.");
+    })
+})
 
 const signUpButtons = document.querySelectorAll(".btn");
 signUpButtons.forEach(button => {
     button.addEventListener("contextmenu", event => {
         event.preventDefault();
+        event.stopPropagation();
         window.alert("Please have contact & credit card information readily available.");
     });
 });
 
-const paragraphText = document.querySelectorAll("p");
-paragraphText.forEach(p => {
-    p.addEventListener("keydown", event => {
-        if (event.keyCode === 72) {
-            event.target.style.color = "red";
-        }
+const paragraphs = document.querySelectorAll("p");
+paragraphs.forEach(p => {
+    p.addEventListener("resize", event => {
+        event.target.style.color = "red";
+        // if (event.keyCode === 72) {
+        //     event.target.selection.style.color = "red";
+        // }
     });
 });
